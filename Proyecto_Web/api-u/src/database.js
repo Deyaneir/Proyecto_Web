ef018b2754c583e52e36302060ebeb85345d8b8b
Santy <santiago.vargas@epn.edu.ec>
2025-11-28 21:21:01 -0500

    Cambios de database

--- Proyecto_Web/api-u/src/database.js
+++ Proyecto_Web/api-u/src/database.js
@@ -4,14 +4,14 @@ mongoose.set('strictQuery', true);
 
 const connection = async () => {
     try {
-        const { connection: dbConnection } = await mongoose.connect(process.env.MONGO_URI_ATLAS, {
+        const { connection: dbConnection } = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
         console.log(`✅ Database connected on ${dbConnection.host}:${dbConnection.port}`);
+        console.log("✅ MongoDB conectado correctamente");
     } catch (error) {
         console.error("❌ Error connecting to MongoDB:", error);
     }
 };
-
 export default connection;
--- Proyecto_Web/api-u/src/server.js
+++ Proyecto_Web/api-u/src/server.js
@@ -23,17 +23,6 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-// ✅ Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("✅ MongoDB conectado correctamente"))
-.catch(err => {
-    console.error("❌ Error al conectar MongoDB:", err.message);
-    process.exit(1);
-});
-
 // Variables globales
 app.set("port", process.env.PORT || 3000);
 
